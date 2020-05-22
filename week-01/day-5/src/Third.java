32a7d58a3f36b47dda4afea0da7e8b965150d2e0
Mila Karous <mila@karous.eu>
2020-05-22 16:09:51 +0200

    arrays started

---
+++ week-01/day-5/src/Third.java
@@ -0,0 +1,8 @@
+public class Third {
+    public static void main(String[] args) {
+        int[] numbers = {4, 5, 6, 7};
+
+        // Print the second element of the array
+        System.out.println("The third element is " + numbers[2]);
+    }
+}
