b1c237484a73b4e3218a6eb3d24c7de3a825fcd4
Mila Karous <mila@karous.eu>
2020-06-03 17:11:32 +0200

    Delete App.java

--- week-03/Day-3/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Sharpie bluSharpie = new Sharpie("blue",100f);
-        System.out.println(bluSharpie.width);
-        System.out.println(bluSharpie.inkAmount);
-    }
-}
