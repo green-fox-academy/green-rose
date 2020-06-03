d08b77eb3f6e8acffc297e1b0fa068bf375991e3
Mila Karous <mila@karous.eu>
2020-06-03 15:57:16 +0200

    Create FleetOfThings.java

---
+++ week-03/Day-3/src/FleetOfThings.java
@@ -0,0 +1,30 @@
+public class FleetOfThings {
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // - You have the `Thing` class
+        // - You have the `Fleet` class
+        // - You have the `FleetOfThings` class with a `main` method
+        // - Download those, use those
+        // - In the main method create a fleet
+        Fleet myFleet = new Fleet();
+        Thing firstThing = new Thing("Get milk");
+        myFleet.add(firstThing);
+        Thing secondThing = new Thing("Remove the obstacles");
+        myFleet.add(secondThing);
+        Thing thirdThing = new Thing("Stand up");
+        thirdThing.complete();
+        myFleet.add(thirdThing);
+        Thing fourthThing = new Thing("Eat lunch");
+        myFleet.add(fourthThing);
+        fourthThing.complete();
+
+        // - Achieve this output:
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        System.out.println(myFleet.toString());
+    }
+}
