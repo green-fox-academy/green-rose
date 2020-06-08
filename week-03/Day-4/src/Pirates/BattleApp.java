6a1eb30365c131806e71737e194ba34dcf880243
Mila Karous <mila@karous.eu>
2020-06-08 15:17:34 +0200

    Update BattleApp.java

--- week-03/Day-4/src/Pirates/BattleApp.java
+++ week-03/Day-4/src/Pirates/BattleApp.java
@@ -10,23 +10,6 @@ public class BattleApp {
         white.setCaptain("Jack One Eye");
         white.fillShip();
 
-        black.crew.get(0).drinkSomeRum();
-        black.crew.get(0).drinkSomeRum();
-        black.crew.get(0).drinkSomeRum();
-        black.crew.get(0).howsItGoingMate();
-        black.crew.get(0).drinkSomeRum();
-        black.crew.get(0).setParrot(true);
-        black.crew.get(0).drinkSomeRum();
-        black.crew.get(0).howsItGoingMate();
-        black.crew.get(0).howsItGoingMate();
-        black.crew.get(1).die();
-        black.crew.get(1).brawl(black.crew.get(3));
-        black.crew.get(2).setParrot(true);
-        black.crew.get(2).brawl(black.crew.get(3));
-        black.captain.drinkSomeRum();
-        black.captain.drinkSomeRum();
-        black.captain.drinkSomeRum();
-
         System.out.println(black);
         System.out.println(white);
 
