d3582af1008e670d5fbb23e7d84c89632f48b65f
Mila Karous <mila@karous.eu>
2020-06-08 16:33:56 +0200

    Create F16.java

---
+++ week-04/Day-1/src/AircraftCarrier/F16.java
@@ -0,0 +1,16 @@
+package AircraftCarrier;
+
+public class F16 extends Aircraft{
+
+    public F16(){
+        super();
+        this.maxAmmo=8;
+        this.baseDamage=30;
+        this.type="F16";
+    }
+
+    @Override
+    boolean isPriority() {
+        return false;
+    }
+}
