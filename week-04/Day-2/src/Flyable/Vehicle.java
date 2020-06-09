82523d1313b527608b00b2ae24fa3c5b4960fcef
Mila Karous <mila@karous.eu>
2020-06-09 16:25:03 +0200

    Flyable

---
+++ week-04/Day-2/src/Flyable/Vehicle.java
@@ -0,0 +1,9 @@
+package Flyable;
+
+import java.awt.*;
+
+public abstract class Vehicle {
+    protected String name;
+    Color color;
+    boolean motor;
+}
--- week-04/Day-2/src/main/java/animals/Bird.java
+++ week-04/Day-2/src/main/java/animals/Bird.java
@@ -1,11 +1,28 @@
 package main.java.animals;
 
-public class Bird extends EggBorns  {
+import Flyable.Flyable;
+
+public class Bird extends EggBorns implements Flyable {
     public Bird(String name){
         this.name =name;
     }
     @Override
     public String getName() {
         return this.name;
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void takeOff() {
+
     }
 }
