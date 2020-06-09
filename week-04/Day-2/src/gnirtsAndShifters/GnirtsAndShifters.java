2b039c7f2fbe3ce414c969a8aec72f935b8f030c
Mila Karous <mila@karous.eu>
2020-06-09 14:36:51 +0200

    Create GnirtsAndShifters.java

---
+++ week-04/Day-2/src/gnirtsAndShifters/GnirtsAndShifters.java
@@ -0,0 +1,12 @@
+package gnirtsAndShifters;
+
+public class GnirtsAndShifters {
+    public static void main(String[] args) {
+        Gnirts g = new Gnirts("example");
+        System.out.println(g.charAt(1));
+        Shifter s = new Shifter("example", 2);
+        System.out.println(s.charAt(0));
+
+
+    }
+}
