da36e4e811924832f91d0eb9403801ae7ff28b1a
Mila Karous <mila@karous.eu>
2020-06-29 16:28:58 +0200

    Update Streams.java

--- week-05/Day-1/src/Streams.java
+++ week-05/Day-1/src/Streams.java
@@ -1,7 +1,13 @@
-import org.w3c.dom.ls.LSOutput;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Streams {
@@ -21,7 +27,7 @@ public class Streams {
         List<Integer> numbers3 = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
         System.out.println(numbers1.stream().filter(x -> x % 2 != 0).mapToInt(x -> x).sum());
         //Exercise6
-        String string = "Vole,Žába";
+        String string = "Vole, Žába na Prameni a kráva v potoce!!!";
         string.chars().mapToObj(c -> (char) c).filter(c->Character.isUpperCase(c)).forEach(System.out::println);
         //Exercise7
         List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
@@ -29,7 +35,20 @@ public class Streams {
         cities.stream().filter(s->s.charAt(0)==par).forEach(System.out::println);
         //Exercise8
         List<Character> chars = Arrays.asList('a','c','d','e','f');
-        String s ="";
-        s+=chars.stream().forEach(c->c);
+        System.out.println(chars.stream().map(c->c.toString()).collect(Collectors.joining()));
+        //Exercise9
+        string.chars().mapToObj(c->(char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c->1))).entrySet().stream().forEach(System.out::println);
+        //Exercise10
+        //Exercise11
+        try {
+            Stream.of(Files.readString(Paths.get("text.txt")).split("\\W+")).map(word->new String(word))
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c->1)))
+                    .entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(100).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("no file");
+        }
+
     }
 }
