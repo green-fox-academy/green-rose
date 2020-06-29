dd207d98a86333adfa8b1ec228f0f3c5859c800a
Mila Karous <mila@karous.eu>
2020-06-29 10:47:34 +0200

    Update Streams.java

--- week-05/Day-1/src/Streams.java
+++ week-05/Day-1/src/Streams.java
@@ -6,8 +6,16 @@ import java.util.List;
 public class Streams {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-        numbers.stream().filter(x->x%2!=0).forEach(System.out::println);
-        numbers.stream().filter(x->x>0).mapToInt(x->x*x).forEach(System.out::println);
+        List<Integer> numbers1 = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        //Exercise1
+        numbers1.stream().filter(x->x%2!=0).forEach(System.out::println);
+        //Exercise2
+        numbers1.stream().filter(x->x>0).mapToInt(x->x*x).forEach(System.out::println);
+        //Exercise3
+        List<Integer>numbers2 = Arrays.asList(3, 9, 2, 8, 6, 5);
+        numbers2.stream().mapToInt(x->x*x).filter(x->x>20).forEach(System.out::println);
+        //Exercise4
+        System.out.println( numbers1.stream().filter(x->x%2==0).mapToInt(x->x).average());
+
     }
 }
