b0c0f9d3dc5b27bc0536ee402d8df390efd024b0
Mila Karous <mila@karous.eu>
2020-07-18 15:57:48 +0200

    n:n table

--- week-08/thereddit/src/main/java/com/gfa/thereddit/repositories/PostRepository.java
+++ week-08/thereddit/src/main/java/com/gfa/thereddit/repositories/PostRepository.java
@@ -1,8 +1,6 @@
 package com.gfa.thereddit.repositories;
 
 import com.gfa.thereddit.models.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
